59980dcd167bc86d53d7f275fb10fc61023cb1ee
Doug <27059467+doug-newton@users.noreply.github.com>
2023-02-25 16:48:11 +0200

    simplify controller registration

--- server/routes.ts
+++ server/routes.ts
@@ -7,13 +7,9 @@ import UserCtrl from './controllers/user';
 const setRoutes = (app: Application): void => {
   const router = Router();
 
-  const catCtrl = new CatCtrl();
-  const dogCtrl = new DogCtrl();
-  const userCtrl = new UserCtrl();
-
-  catCtrl.registerRoutes(router);
-  userCtrl.registerRoutes(router);
-  dogCtrl.registerRoutes(router);
+  new CatCtrl().registerRoutes(router);
+  new DogCtrl().registerRoutes(router);
+  new UserCtrl().registerRoutes(router);
 
   // Apply the routes to our application with the prefix /api
   app.use('/api', router);
